08fb5acf03a39371273f680d57c88cb5e3852ca4
mrtstg <mrtstgdev@gmail.com>
2025-08-06 08:24:15 +0300

    feat: added experimental implementation of oauth2 token storage for apps

---
+++ src/Api/Keycloak/Token.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE FlexibleContexts #-}
+module Api.Keycloak.Token
+  ( TokenVariableFunctions(..)
+  , TokenVariable
+  , createTokenVar
+  , withTokenVariable
+  ) where
+
+import           Control.Concurrent.STM
+import           Control.Monad.Reader
+
+data TokenVariableFunctions a = TokenFunctions
+  { tokenValidateF :: a -> IO Bool
+  , tokenIssueF    :: IO (Either String a)
+  }
+
+class HasTokenVariable t where
+  getTokenVariable :: t -> TokenVariable a
+  getTokenFunctions :: t -> TokenVariableFunctions a
+
+type TokenVariable a = TVar (Maybe a)
+
+createTokenVar :: IO (TokenVariable a)
+createTokenVar = newTVarIO Nothing
+
+withTokenVariable :: (MonadIO m, HasTokenVariable t, MonadReader t m) => (a -> m b) -> m (Either String b)
+withTokenVariable f = do
+  (TokenFunctions validateToken issueToken) <- asks getTokenFunctions
+  v <- asks getTokenVariable
+  currentToken' <- liftIO $ readTVarIO v
+  case currentToken' of
+    Nothing -> do
+      newToken <- liftIO issueToken
+      case newToken of
+        (Left e) -> pure $ Left e
+        (Right tokenV) -> do
+          (liftIO . atomically) $ writeTVar v (Just tokenV)
+          withTokenVariable f
+    (Just tokenV) -> do
+      tokenValid <- liftIO $ validateToken tokenV
+      if not tokenValid then do
+        newToken <- liftIO issueToken
+        case newToken of
+          (Left e) -> pure $ Left e
+          (Right tokenV) -> do
+            (liftIO . atomically) $ writeTVar v (Just tokenV)
+            withTokenVariable f
+      else do
+        res <- f tokenV
+        (pure . Right) res
